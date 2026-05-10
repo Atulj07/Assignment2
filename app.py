ecbe743d5b13ff5251daef847db383efad9f6d58
Atulj07 <atuljagtap093@gmail.com>
2026-05-10 16:02:51 +0530

    Added code

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 print("Hello, World!")
+
+print("Git Project! change 1")
+
+print("Git Project! change 2")
+
+print("Git Project! change 3")
+
+print("Git Project! change 4 stash")
+
+print("Git Project! change 5 buggy code reset")
+
+print("Git Project! change 6 buggy code revert")
